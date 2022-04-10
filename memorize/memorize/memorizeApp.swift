78203d4b1bffccc1ce808954c8705d7f5cc5f68f
Dmitry Kurski <melancholygaze@gmail.com>
2022-04-10 17:47:19 +0200

    [+] Added project folder.

---
+++ memorize/memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Dmitry Kurski on 10.04.22.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
