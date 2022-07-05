64e2a829c7f26ba14b44019298b7f2d9f98e015e
“melancholy13” <xxxiamnotokeyxxx@gmail.com>
2022-07-05 19:42:08 +0200

    [*] Updated game logic.

--- memorize/memorize/MemoryGame.swift
+++ memorize/memorize/MemoryGame.swift
@@ -7,23 +7,28 @@
 
 import Foundation
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
   
   private(set) var cards: Array<Card>
+  private var indexOfOneAndOnlyFaceUpCard: Int?
   
   mutating func choose(_ card: Card) {
-    let chosenIndex = index(of: card)
-    cards[chosenIndex].isFaceUp.toggle()
-  }
-  
-  func index(of card: Card) -> Int {
-    for index in 0..<cards.count {
-      if cards[index].id == card.id {
-        return index
+    if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }), !cards[chosenIndex].isFaceUp, !cards[chosenIndex].isMatched {
+      if let potentialMatchIndex = indexOfOneAndOnlyFaceUpCard {
+        if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+          cards[chosenIndex].isMatched = true
+          cards[potentialMatchIndex].isMatched = true
+        }
+        indexOfOneAndOnlyFaceUpCard = nil
+      } else {
+        for index in cards.indices {
+          cards[index].isFaceUp = false
+        }
+        indexOfOneAndOnlyFaceUpCard = chosenIndex
       }
+      
+      cards[chosenIndex].isFaceUp.toggle()
     }
-    
-    return 0
   }
   
   init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
