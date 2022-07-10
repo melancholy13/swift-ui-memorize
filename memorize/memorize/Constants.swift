2ae9c6dc9c0da827959ebaa688ba283ea5aca30e
melancholy13 <xxxiamnotokeyxxx@gmail.com>
2022-07-10 02:10:24 +0200

    [*] Updated layout. [+] Added AspectVGrid.

--- memorize/memorize/Constants.swift
+++ memorize/memorize/Constants.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 struct DrawingConstants {
-  static let cornerRadius: CGFloat = 20
+  static let cornerRadius: CGFloat = 10
   static let lineWidth: CGFloat = 3
-  static let fontScale: CGFloat = 0.8
+  static let fontScale: CGFloat = 0.75
 }
